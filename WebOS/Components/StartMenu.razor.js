f9d51e345e01d7aced9268e2c723033534b1381c
thebluearchive <klajdi.gjonaj@gmail.com>
2022-03-17 18:25:57 -0400

    Added Start Menu

---
+++ WebOS/Components/StartMenu.razor.js
@@ -0,0 +1,12 @@
+﻿
+export function getActiveElement() {
+    var elem = document.activeElement;
+    console.log("start-menu has focus: " + document.hasFocus(document.getElementById("start-menu")))
+    console.log("active element: " + elem.id)
+    if (elem != null) {
+        return elem.id;
+    }
+    else {
+        return null;
+    }
+}
