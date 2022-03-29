8c0a05a986c934de51cf7fef87db57ccd2b5520c
thebluearchive <klajdi.gjonaj@gmail.com>
2022-03-29 01:23:04 -0400

    Added draggable window system

---
+++ WebOS/Components/Window.razor.js
@@ -0,0 +1,20 @@
+﻿
+var DotNetHelper;
+
+// Window drag code
+export function dragStart(dotNetHelper) {
+    document.addEventListener('mousemove', dragMove);
+    document.addEventListener('mouseup', dragEnd);
+    DotNetHelper = dotNetHelper;
+};
+
+export function dragMove(e) {
+    DotNetHelper.invokeMethodAsync('MouseMove', e.clientX, e.clientY);
+};
+
+export function dragEnd(e) {
+    
+    document.removeEventListener('mousemove', dragMove);
+    document.removeEventListener('mouseup', dragEnd);
+    //DotNetHelper.invokeMethodAsync('MouseUp', e.clientX, e.clientY);
+}
