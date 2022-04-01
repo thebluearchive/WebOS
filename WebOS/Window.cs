b1bb4dc9729d94bd795f5608a586f8c2dd8d4296
thebluearchive <klajdi.gjonaj@gmail.com>
2022-03-31 20:04:51 -0400

    Task manager and window container are now synchronized

---
+++ WebOS/Window.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace WebOS
+{
+    public class Window
+    {
+        public RenderFragment ChildContent { get; set; }
+        public string Width { get; set; } = "400px";
+        public string Height { get; set; } = "400px";
+        public double XPos { get; set; } = 200;
+        public double YPos { get; set; } = 200;
+        public int ZIndex { get; set; } = 0;
+        public string Name { get; set; } = "";
+        public bool Visible { get; set; } = true;
+        public event EventHandler? OnWindowStateChanged;
+        public event EventHandler? OnWindowClosed;
+
+        public WindowState windowState = WindowState.Windowed;
+
+        public void Minimize()
+        {
+            if (windowState == WindowState.Minimized)
+            {
+                windowState = WindowState.Windowed;
+                Visible = true;
+            }
+            else
+            {
+                windowState = WindowState.Minimized;
+                Visible = false;
+            }
+            OnWindowStateChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void Maximize()
+        {
+            if (windowState == WindowState.Windowed)
+            {
+                windowState = WindowState.Maximized;
+            }
+            else if (windowState == WindowState.Maximized)
+            {
+                windowState = WindowState.Windowed;
+            }
+            XPos = 0;
+            YPos = 0;
+            Width = "100%";
+            Height = "100%";
+            OnWindowStateChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void Close()
+        {
+            // It is the responsibility of the window manager to close
+            // the window
+            OnWindowClosed?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
--- WebOS/WindowManager.cs
+++ WebOS/WindowManager.cs
@@ -4,38 +4,40 @@ namespace WebOS
 {
     public interface IWindowManager
     {
-        public Dictionary<Window, int> OpenWindows { get; set; }
-        void OpenNewWindow();
+        public HashSet<Window> OpenWindows { get; set; }
+        public event EventHandler OnOpenWindowsChanged;
+        void OpenNewWindow(Window window);
         void CloseWindow(Window window);
         void BringToTop(Window window);
     }
 
     public class WindowManager : IWindowManager
     {
-        // Stores windows along with their associated window heights
-        public Dictionary<Window, int> OpenWindows { get; set; } = new();
+        public HashSet<Window> OpenWindows { get; set; } = new();
         private int maxWindowHeight = 0;
+        public event EventHandler? OnOpenWindowsChanged;
 
-        public void OpenNewWindow()
+        public void OpenNewWindow(Window window)
         {
-            Window window = new();
+            window.OnWindowClosed += (obj, args) => CloseWindow((Window)obj);
             maxWindowHeight += 1;
-            OpenWindows.Add(window, maxWindowHeight);
+            OpenWindows.Add(window);
+            OnOpenWindowsChanged?.Invoke(this, new EventArgs());
         }
 
         public void CloseWindow(Window window)
         {
             OpenWindows.Remove(window);
+            OnOpenWindowsChanged?.Invoke(this, new EventArgs());
         }
 
         public void BringToTop(Window window)
         {
             // Bringing to top is only necessary if window is not already on top
-            int currentWindowHeight = OpenWindows[window];
-            if (currentWindowHeight == maxWindowHeight) { return; }
+            if (window.ZIndex == maxWindowHeight) { return; }
 
             maxWindowHeight += 1;
-            OpenWindows[window] = maxWindowHeight;
+            window.ZIndex = maxWindowHeight;
 
             // Keep the window height from growing too large
             if (maxWindowHeight >= 1000)
@@ -43,16 +45,16 @@ namespace WebOS
                 ResetWindowHeights();
             }
         }
+
         private void ResetWindowHeights()
         {
             var windowList = OpenWindows.ToList();
-            windowList.Sort((pair1, pair2) => pair1.Value.CompareTo(pair2.Value));
+            windowList.Sort((window1, window2) => window1.ZIndex.CompareTo(window2.ZIndex));
 
             for (int i = 0; i < windowList.Count; i++)
             {
-                Window window = windowList[i].Key;
-                int height = windowList[i].Value;
-                OpenWindows[window] = i;
+                Window window = windowList[i];
+                window.ZIndex = i;
             }
         }
     }
