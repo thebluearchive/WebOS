0b668ad143b3d4db195759503e11618bd8ac6785
thebluearchive <klajdi.gjonaj@gmail.com>
2022-04-12 21:04:43 -0400

    Added Text Editor

---
+++ WebOS/Components/TextEditor.razor.js
@@ -0,0 +1,9 @@
+﻿
+export function createCodeMirror(container) {
+    var myCodeMirror = CodeMirror(container, {
+        value: "this is a test document\nDoes it display properly?",
+        indentUnit: 4,
+        lineNumbers: true//,
+        //theme: "the-matrix"
+    });
+}
